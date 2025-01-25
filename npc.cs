39abdbb2fec4745228a8bc0d1521ea397a20ea12
m-m222m-m <art.savkov1210@gmail.com>
2025-01-25 18:54:00 +0700

    Add files via upload

---
+++ npc.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npc : MonoBehaviour
+{
+    public int health=10;
+    public int lavel=554;
+    public float speed=1.9f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
